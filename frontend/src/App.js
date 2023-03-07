abc33601bd2d778cbf44816ea9a68d9a01eebffc
SidP18 <SidharthPrakasan23@gmail.com>
2023-03-07 14:36:18 -0500

    Prey

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
   <div className="top">
     <input 
     type="text" 
-    placeholder="Add ToDos..." 
+    placeholder="Add ToDos" 
     value={text}
     onChange={(e) => setText(e.target.value)}
     />
